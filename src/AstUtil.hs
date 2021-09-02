de85b9a3fcf582c94d08ff82a70e859a5d6c7b6b
William McPherson <willmcpherson2@gmail.com>
2021-09-02 18:56:51 +1000

    Rewrite qualification code

--- src/AstUtil.hs
+++ src/AstUtil.hs
@@ -1,7 +1,20 @@
-module AstUtil (mapExprs, foldExprs, mapTypes, mapPats, foldPat, inPat) where
+module AstUtil
+  ( mapExprs
+  , foldExprs
+  , mapTypes
+  , mapPats
+  , foldPat
+  , inPat
+  , foldExprsLams
+  , foldLams
+  , mapExprsExpr
+  , mapScopes
+  ) where
 
 import Ast
-import Data.Either (fromRight, rights)
+import Data.Either (fromRight)
+import Data.Tuple.Extra
+import Error (Fallible)
 import Sexpr (Part)
 
 mapPats :: (Pat -> Pat) -> Ast -> Ast
@@ -92,32 +105,101 @@ mapExprsLam f (Lam id pat expr) = Lam id pat (mapExprsExpr f expr)
 
 --------------------------------------------------------------------------------
 
-foldExprs :: (Expr -> b -> b) -> b -> Ast -> b
-foldExprs f y (Ast items) = foldExprsItems f y (rights items)
+mapScopes :: ([Part] -> Lam -> Lam) -> Ast -> Ast
+mapScopes f (Ast items) = Ast $ map (fmap $ mapScopesItem f) items
 
-foldExprsItems :: (Expr -> b -> b) -> b -> [Item] -> b
-foldExprsItems f y items = case items of
-  [] -> y
-  item : items -> foldExprsItem f (foldExprsItems f y items) item
+mapScopesItem :: ([Part] -> Lam -> Lam) -> Item -> Item
+mapScopesItem f item = case item of
+  ItemLet (Let (Right name) expr) ->
+    ItemLet $ Let (Right name) (mapScopesExpr f [name] expr)
+  _ -> item
 
-foldExprsItem :: (Expr -> b -> b) -> b -> Item -> b
-foldExprsItem f y item = case item of
-  ItemLet (Let _ expr) -> foldExprsExpr f y expr
-  _ -> y
+mapScopesExpr :: ([Part] -> Lam -> Lam) -> [Part] -> Expr -> Expr
+mapScopesExpr f qualifier (Expr ty val) = Expr ty $ case val of
+  Right (ValLam lam) -> Right $ ValLam $ mapScopesLam f qualifier lam
+  Right (ValCase (Case caseExpr lams)) -> Right $ ValCase $ Case
+    (mapScopesExpr f qualifier caseExpr)
+    (map (fmap $ mapScopesLam f qualifier) lams)
+  Right (ValApp (AppVal l r)) -> Right $ ValApp $ AppVal
+    (mapScopesExpr f qualifier l)
+    (mapScopesExpr f qualifier r)
+  _ -> val
 
-foldExprsExpr :: (Expr -> b -> b) -> b -> Expr -> b
-foldExprsExpr f y expr@(Expr _ val) = case val of
-  Right (ValLam (Lam _ _ lamExpr)) -> f expr (foldExprsExpr f y lamExpr)
-  Right (ValCase (Case caseExpr lams)) ->
-    f expr (foldExprsExpr f (foldExprsLams f y (rights lams)) caseExpr)
-  Right (ValApp (AppVal l r)) ->
-    f expr (foldExprsExpr f (foldExprsExpr f y l) r)
-  _ -> f expr y
+mapScopesLam :: ([Part] -> Lam -> Lam) -> [Part] -> Lam -> Lam
+mapScopesLam f qualifier lam =
+  let
+    qualifier' = qualifier ++ [id]
+    Lam id pat expr = f qualifier' lam
+  in Lam id pat $ mapScopesExpr f qualifier' expr
 
-foldExprsLams :: (Expr -> b -> b) -> b -> [Lam] -> b
-foldExprsLams f y lams = case lams of
-  [] -> y
-  lam : lams -> foldExprsLam f (foldExprsLams f y lams) lam
+--------------------------------------------------------------------------------
 
-foldExprsLam :: (Expr -> b -> b) -> b -> Lam -> b
-foldExprsLam f y (Lam _ _ expr) = foldExprsExpr f y expr
+foldExprs :: (Expr -> a -> (Expr, a)) -> a -> Ast -> (Ast, a)
+foldExprs f x (Ast items) =
+  let (items', x') = foldExprsItems f x items in (Ast items', x')
+
+foldExprsItems
+  :: (Expr -> a -> (Expr, a)) -> a -> [Fallible Item] -> ([Fallible Item], a)
+foldExprsItems f x items = case items of
+  [] -> ([], x)
+  item : items ->
+    let
+      (item', x') = foldExprsItem f x item
+      (items', x'') = foldExprsItems f x' items
+    in (item' : items', x'')
+
+foldExprsItem
+  :: (Expr -> a -> (Expr, a)) -> a -> Fallible Item -> (Fallible Item, a)
+foldExprsItem f x item = case item of
+  Right (ItemLet (Let name expr)) ->
+    let (expr', x') = foldExprsExpr f x expr
+    in (Right $ ItemLet $ Let name expr', x')
+  _ -> (item, x)
+
+foldExprsExpr :: (Expr -> a -> (Expr, a)) -> a -> Expr -> (Expr, a)
+foldExprsExpr f x expr = case f expr x of
+  (Expr ty (Right (ValApp (AppVal l r))), x') ->
+    let
+      (l', x'') = foldExprsExpr f x' l
+      (r', x''') = foldExprsExpr f x'' r
+    in (Expr ty (Right $ ValApp $ AppVal l' r'), x''')
+  (Expr ty (Right (ValLam (Lam id pat lamExpr))), x') ->
+    let (lamExpr', x'') = foldExprsExpr f x' lamExpr
+    in (Expr ty (Right $ ValLam $ Lam id pat lamExpr'), x'')
+  (Expr ty (Right (ValCase (Case caseExpr lams))), x') ->
+    let
+      (caseExpr', x'') = foldExprsExpr f x' caseExpr
+      (lams', x''') = foldExprsLams f x'' lams
+    in (Expr ty (Right $ ValCase $ Case caseExpr' lams'), x''')
+  _ -> f expr x
+
+foldExprsLams
+  :: (Expr -> a -> (Expr, a)) -> a -> [Fallible Lam] -> ([Fallible Lam], a)
+foldExprsLams f x lams = case lams of
+  [] -> ([], x)
+  lam : lams ->
+    let
+      (lam', x') = foldExprsLam f x lam
+      (lams', x'') = foldExprsLams f x' lams
+    in (lam' : lams', x'')
+
+foldExprsLam
+  :: (Expr -> a -> (Expr, a)) -> a -> Fallible Lam -> (Fallible Lam, a)
+foldExprsLam f x lam = case lam of
+  Right (Lam id pat expr) ->
+    let (expr', x') = f expr x in (Right $ Lam id pat expr', x')
+  _ -> (lam, x)
+
+--------------------------------------------------------------------------------
+
+foldLams
+  :: (Lam -> a -> (Lam, a)) -> a -> [Fallible Lam] -> ([Fallible Lam], a)
+foldLams f x lams = case lams of
+  [] -> ([], x)
+  lam : lams ->
+    let
+      (lam', x') = case lam of
+        Right lam -> first Right (f lam x)
+        Left _ -> (lam, x)
+      (lams', x'') = foldLams f x' lams
+    in (lam' : lams', x'')
--- src/Qualify.hs
+++ src/Qualify.hs
@@ -1,133 +1,61 @@
 module Qualify (qualify) where
 
 import Ast
-  ( AppVal(AppVal)
-  , Ast(Ast)
+  ( Ast
   , Case(Case)
   , Expr(Expr)
-  , Item(ItemLet)
   , Lam(Lam)
-  , Let(Let)
-  , Pat(PatBinder, PatCtor)
+  , Pat(PatCtor)
   , Type(TypeName)
-  , Val(ValApp, ValCase, ValCtor, ValLam, ValName)
+  , Val(ValCase, ValCtor, ValLam, ValName)
   )
-import AstUtil (foldPat, inPat, mapExprs, mapPats, mapTypes)
-import Data.Either (fromRight)
+import AstUtil
+  ( foldExprs
+  , foldLams
+  , inPat
+  , mapExprs
+  , mapExprsExpr
+  , mapPats
+  , mapScopes
+  , mapTypes
+  )
 import Error (Error(InvalidPath), Fallible)
 import Resolve (resolveMaybe)
 import Sexpr (Part(Anon), Path(Path))
 
 qualify :: Ast -> Ast
-qualify = validate . qualifyScopes . nameScopes
+qualify = validate . qualifyNames . nameScopes
 
 --------------------------------------------------------------------------------
 
 nameScopes :: Ast -> Ast
-nameScopes (Ast items) = Ast $ snd $ foldr nameScopesItem (0, []) items
-
-nameScopesItem
-  :: Fallible Item -> (Int, [Fallible Item]) -> (Int, [Fallible Item])
-nameScopesItem item (id, items) = case item of
-  Right (ItemLet (Let path expr)) ->
-    let (id', expr') = nameScopesExpr id expr
-    in (id', Right (ItemLet $ Let path expr') : items)
-  _ -> (id, item : items)
-
-nameScopesExpr :: Int -> Expr -> (Int, Expr)
-nameScopesExpr id expr = case expr of
-  Expr ty (Right val) ->
-    let (id', val') = nameScopesVal id val in (id', Expr ty $ Right val')
-  _ -> (id, expr)
-
-nameScopesVal :: Int -> Val -> (Int, Val)
-nameScopesVal id val = case val of
-  ValLam lam -> let (id', lam') = nameScopesLam id lam in (id', ValLam lam')
-  ValCase (Case expr lams) ->
-    let
-      (id', expr') = nameScopesExpr id expr
-      (id'', lams') = nameScopesLams id' lams
-    in (id'', ValCase $ Case expr' lams')
-  ValApp (AppVal l r) ->
-    let
-      (id', l') = nameScopesExpr id l
-      (id'', r') = nameScopesExpr id' r
-    in (id'', ValApp $ AppVal l' r')
-  _ -> (id, val)
-
-nameScopesLams :: Int -> [Fallible Lam] -> (Int, [Fallible Lam])
-nameScopesLams id = foldr name (id, [])
+nameScopes = fst . foldExprs nameLam 0
   where
-    name lam (id, lams) = case lam of
-      Right lam ->
-        let (id', lam') = nameScopesLam id lam in (id', Right lam' : lams)
-      Left lam -> (id, Left lam : lams)
-
-nameScopesLam :: Int -> Lam -> (Int, Lam)
-nameScopesLam id (Lam _ pat expr) =
-  let (id', expr') = nameScopesExpr (id + 1) expr
-  in (id', Lam (Anon id) pat expr')
+    nameLam expr n = case expr of
+      Expr ty (Right (ValLam (Lam _ pat expr))) ->
+        (Expr ty $ Right $ ValLam $ Lam (Anon n) pat expr, n + 1)
+      Expr ty (Right (ValCase (Case expr lams))) ->
+        let (lams', n') = foldLams nameCaseLam n lams
+        in (Expr ty $ Right $ ValCase $ Case expr lams', n')
+      _ -> (expr, n)
+    nameCaseLam (Lam _ pat expr) n = (Lam (Anon n) pat expr, n + 1)
 
 --------------------------------------------------------------------------------
 
-qualifyScopes :: Ast -> Ast
-qualifyScopes (Ast items) = Ast $ map (fmap qualifyScopesItem) items
+qualifyNames :: Ast -> Ast
+qualifyNames = mapScopes qualifyScope
 
-qualifyScopesItem :: Item -> Item
-qualifyScopesItem item = case item of
-  ItemLet (Let (Right path) expr) ->
-    ItemLet $ Let (Right path) (qualifyScopesExpr (Path [] path) expr)
-  _ -> item
-
-qualifyScopesExpr :: Path -> Expr -> Expr
-qualifyScopesExpr path expr = case expr of
-  Expr ty (Right val) -> Expr ty (Right $ qualifyScopesVal path val)
+qualifyScope :: [Part] -> Lam -> Lam
+qualifyScope qualifier (Lam id pat expr) = Lam id pat $ case pat of
+  Right pat -> mapExprsExpr (qualifyName qualifier (`inPat` pat)) expr
   _ -> expr
 
-qualifyScopesVal :: Path -> Val -> Val
-qualifyScopesVal path val = case val of
-  ValLam lam -> ValLam $ qualifyNames path lam
-  ValCase (Case expr lams) -> ValCase
-    $ Case (qualifyScopesExpr path expr) (map (fmap $ qualifyNames path) lams)
-  ValApp (AppVal l r) ->
-    ValApp $ AppVal (qualifyScopesExpr path l) (qualifyScopesExpr path r)
+qualifyName :: [Part] -> (Part -> Bool) -> Expr -> Expr
+qualifyName qualifier inPat (Expr ty val) = Expr ty $ case val of
+  Right (ValName (Right (Path valQualifier name))) ->
+    let valQualifier' = if inPat name then qualifier else valQualifier
+    in Right $ ValName $ Right $ Path valQualifier' name
   _ -> val
-
-qualifyNames :: Path -> Lam -> Lam
-qualifyNames (Path qualifier name) (Lam id pat expr) =
-  let
-    path = Path (qualifier ++ [name]) id
-    expr' = qualifyScopesExpr path expr
-    expr'' = fromRight expr' (foldPat qualify expr' <$> pat)
-  in Lam id pat expr''
-  where
-    qualify pat expr = case pat of
-      PatBinder binder ->
-        let path = Path (qualifier ++ [name] ++ [id]) binder
-        in qualifyNamesExpr path expr
-      _ -> expr
-
-qualifyNamesExpr :: Path -> Expr -> Expr
-qualifyNamesExpr path expr = case expr of
-  Expr ty (Right val) -> Expr ty (Right $ qualifyNamesVal path val)
-  _ -> expr
-
-qualifyNamesVal :: Path -> Val -> Val
-qualifyNamesVal path@(Path _ name) val = case val of
-  ValName valPath@(Right (Path [] valName)) ->
-    ValName $ if name == valName then Right path else valPath
-  ValLam lam -> ValLam $ qualifyNamesLam path lam
-  ValCase (Case expr lams) -> ValCase $ Case
-    (qualifyNamesExpr path expr)
-    (map (fmap $ qualifyNamesLam path) lams)
-  ValApp (AppVal l r) ->
-    ValApp $ AppVal (qualifyNamesExpr path l) (qualifyNamesExpr path r)
-  _ -> val
-
-qualifyNamesLam :: Path -> Lam -> Lam
-qualifyNamesLam path@(Path _ name) (Lam id pat expr) = Lam id pat $ case pat of
-  Right pat -> if inPat name pat then expr else qualifyNamesExpr path expr
-  _ -> qualifyNamesExpr path expr
 
 --------------------------------------------------------------------------------
 
